14aac210bbc58c0568f260f75740c105518e122f
cladendas <rvdagi05@yandex.ru>
2020-09-07 15:06:27 +0300

    Создание класса BruteForceOperation и вынос туда логики подбора пароля

---
+++ Week3TestWork/Code/BruteForceOperation.swift
@@ -0,0 +1,82 @@
+//
+//  BruteForceOperation.swift
+//  Week3TestWork
+//
+//  Created by cladendas on 06.09.2020.
+//  Copyright © 2020 E-legion. All rights reserved.
+//
+
+import Foundation
+
+class BruteForceOperation: Operation {
+    
+    let characterArray = Consts.characterArray
+    
+    var password = ""
+    
+    var startIndexArray = [Int]()
+    var endIndexArray = [Int]()
+    var maxIndexArray = 0
+    
+    var result = String()
+    
+    init(password: String) {
+        self.password = password
+    }
+    
+    override func main() {
+        result = bruteForce(startString: "0000", endString: "ZZZZ") ?? "Error"
+    }
+    
+    // Возвращает подобранный пароль
+    private func bruteForce(startString: String, endString: String) -> String? {
+
+        let inputPassword = password
+        var startIndexArray = [Int]()
+        var endIndexArray = [Int]()
+        let maxIndexArray = characterArray.count
+        
+        // Создает массивы индексов из входных строк
+        for char in startString {
+            for (index, value) in characterArray.enumerated() where value == "\(char)" {
+                startIndexArray.append(index)
+            }
+        }
+        for char in endString {
+            for (index, value) in characterArray.enumerated() where value == "\(char)" {
+                endIndexArray.append(index)
+            }
+        }
+        
+        var currentIndexArray = startIndexArray
+        
+        // Цикл подбора пароля
+        while true {
+            
+            // Формируем строку проверки пароля из элементов массива символов
+            let currentPass = self.characterArray[currentIndexArray[0]] + self.characterArray[currentIndexArray[1]] + self.characterArray[currentIndexArray[2]] + self.characterArray[currentIndexArray[3]]
+            
+            // Выходим из цикла если пароль найден, или, если дошли до конца массива индексов
+            if inputPassword == currentPass {
+                return currentPass
+            } else {
+                if currentIndexArray.elementsEqual(endIndexArray) {
+                    break
+                }
+                
+                // Если пароль не найден, то происходит увеличение индекса. Для этого в цикле, начиная с последнего элемента осуществляется проверка текущего значения. Если оно меньше максимального значения (61), то индекс просто увеличивается на 1.
+                //Например было [0, 0, 0, 5] а станет [0, 0, 0, 6]. Если же мы уже проверили последний индекс, например [0, 0, 0, 61], то нужно сбросить его в 0, а "старший" индекс увеличить на 1. При этом далее в цикле проверяется переполение "старшего" индекса тем же алгоритмом.
+                //Таким образом [0, 0, 0, 61] станет [0, 0, 1, 0]. И поиск продолжится дальше:  [0, 0, 1, 1],  [0, 0, 1, 2],  [0, 0, 1, 3] и т.д.
+                for index in (0 ..< currentIndexArray.count).reversed() {
+                    guard currentIndexArray[index] < maxIndexArray - 1 else {
+                        currentIndexArray[index] = 0
+                        continue
+                    }
+                    currentIndexArray[index] += 1
+                    break
+                }
+            }
+        }
+        return nil
+    }
+}
--- Week3TestWork/Code/ViewController.swift
+++ Week3TestWork/Code/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
     private let maxTextLength = Consts.maxTextFieldTextLength
     private var password = ""
     
+    let queue = OperationQueue()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         indicator.isHidden = true
@@ -60,69 +62,20 @@ class ViewController: UIViewController {
     private func start() {
         let startTime = Date()
 
-        let queue = DispatchQueue.global(qos: .userInitiated)
         var result: String?
 
-        queue.async {
-
-            result = self.bruteForce(startString: "0000", endString: "ZZZZ")
-
+        let operation = BruteForceOperation(password: password)
+        
+        operation.completionBlock = {
+            result = operation.result
+            
             DispatchQueue.main.async {
                 self.stop(password: result ?? "Error", startTime: startTime)
-            }
-        }
-    }
-    
-    // Возвращает подобранный пароль
-    private func bruteForce(startString: String, endString: String) -> String? {
-
-        let inputPassword = password
-        var startIndexArray = [Int]()
-        var endIndexArray = [Int]()
-        let maxIndexArray = characterArray.count
-        
-        // Создает массивы индексов из входных строк
-        for char in startString {
-            for (index, value) in characterArray.enumerated() where value == "\(char)" {
-                startIndexArray.append(index)
-            }
-        }
-        for char in endString {
-            for (index, value) in characterArray.enumerated() where value == "\(char)" {
-                endIndexArray.append(index)
+                print("конец")
             }
         }
         
-        var currentIndexArray = startIndexArray
-        
-        // Цикл подбора пароля
-        while true {
-            
-            // Формируем строку проверки пароля из элементов массива символов
-            let currentPass = self.characterArray[currentIndexArray[0]] + self.characterArray[currentIndexArray[1]] + self.characterArray[currentIndexArray[2]] + self.characterArray[currentIndexArray[3]]
-            
-            // Выходим из цикла если пароль найден, или, если дошли до конца массива индексов
-            if inputPassword == currentPass {
-                return currentPass
-            } else {
-                if currentIndexArray.elementsEqual(endIndexArray) {
-                    break
-                }
-                
-                // Если пароль не найден, то происходит увеличение индекса. Для этого в цикле, начиная с последнего элемента осуществляется проверка текущего значения. Если оно меньше максимального значения (61), то индекс просто увеличивается на 1.
-                //Например было [0, 0, 0, 5] а станет [0, 0, 0, 6]. Если же мы уже проверили последний индекс, например [0, 0, 0, 61], то нужно сбросить его в 0, а "старший" индекс увеличить на 1. При этом далее в цикле проверяется переполение "старшего" индекса тем же алгоритмом.
-                //Таким образом [0, 0, 0, 61] станет [0, 0, 1, 0]. И поиск продолжится дальше:  [0, 0, 1, 1],  [0, 0, 1, 2],  [0, 0, 1, 3] и т.д.
-                for index in (0 ..< currentIndexArray.count).reversed() {
-                    guard currentIndexArray[index] < maxIndexArray - 1 else {
-                        currentIndexArray[index] = 0
-                        continue
-                    }
-                    currentIndexArray[index] += 1
-                    break
-                }
-            }
-        }
-        return nil
+        self.queue.addOperation(operation)
     }
     
     //Обновляем UI
